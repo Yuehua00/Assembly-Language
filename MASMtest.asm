fe81ba965e523e58b14fca8943dd7bbbf2d6a344
Yuehua00 <113094262+Yuehua00@users.noreply.github.com>
2023-11-03 10:06:13 +0800

    Add files via upload

---
+++ MASMtest.asm
@@ -0,0 +1,28 @@
+; Program Description: A = ( A + B ) - ( C + D )
+; Author: Slider
+; Creation Date: 2023/11/2
+; Revisions: 
+; Date:              Modified by:
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.data
+
+.code
+main proc 
+	mov eax, 4
+	mov ebx, 3
+	mov ecx, 2
+	mov edx, 1
+
+	add eax, ebx
+	add ecx, edx
+	sub eax, ecx
+
+	invoke ExitProcess, 0
+
+main endp
+end main
