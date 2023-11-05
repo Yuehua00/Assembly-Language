b757915db0e38d8861b3e7b575ebda5e7fe83dbf
Yuehua00 <113094262+Yuehua00@users.noreply.github.com>
2023-11-05 10:00:45 +0800

    Update 4-1.asm

--- U4/4-1.asm
+++ U4/4-1.asm
@@ -1,8 +1,10 @@
 ; Converting from Big Endian to Little Endian
 
-; Program Description : Write a program with a loop and indexed addressing that exchanges every pair of values in an array with an even number of elements. Therefore, item i will exchange with i+1, and item i+2 will exchange with item i+3, and so on.
+; Program Description : Write a program that uses the variables below and MOV instructions to copy 
+;                       the value from bigEndian to littleEndian, revirsing the order of the bytes.
+;                       The number's 32-bit value is understood to be 12345678h.
 ; Author : Slider
-; Creation Date : 2023/22/14
+; Creation Date : 2023/11/5
 ; Revisions :
 ; Date :                          Modified by : 
 
@@ -11,25 +13,16 @@
 .stack 4096
 ExitProcess PROTO, dwExitCode:DWORD
 
-PWORD TYPEDEF PTR WORD
-
 .data
-	array WORD 1000h, 2000h, 3000h, 4000h, 5000h, 6000h
-	times DWORD LENGTHOF array / 2
+	bigEndian BYTE 12h, 34h, 56h, 78h
+	littleEndian DWORD ?
 
 .code
 main PROC
-
-	mov ECX, times
-	mov esi, 0
-
-	L1:
-		mov ax, array[esi]
-		xchg ax, array[esi+2]
-		xchg array[esi], ax
-		add esi, TYPE array * 2
-		loop L1
+	mov eax, DWORD PTR bigEndian
+	mov littleEndian, eax
 	
+
 	INVOKE ExitProcess, 0
 main ENDP
 END main
